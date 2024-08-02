560ea0f1c95b09d71394c598c11c25315af6e5a9
Ashwini Krishnaji Rao <ashwinikrishnajirao@gmail.com>
2024-08-02 22:28:20 +0530

    Landing Page customized

--- sign-language-interpreter/src/home.js
+++ sign-language-interpreter/src/home.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './home.css'; // Optional: Import an external CSS file for additional styling
+import './home.css'; // Importing the CSS file
+import LOGO from './LOGO.png'; // Correcting the import
 
 const Home = () => {
   return (
     <div className="landing-page">
+      <img src={LOGO} alt="college logo" />
       <h1>JSS Academy of Technical Education, Bengaluru</h1>
       <h2>COMPUTER GRAPHICS AND IMAGE PROCESSING LABORATORY (21CSL66)</h2>
       <h3>
         PROJECT: <Link to="/app" className="project-link">SIGN LANGUAGE INTERPRETER</Link>
       </h3>
-      <p>BY: ASHWINI KRISHNAJI RAO (1JS21CS030)</p>
-      <p>AISHWARYA S PATIL (1JS21CS013)</p>
-      <p>GUIDED BY: MRS. RASHMI B N, ASST PROF, DEPT OF CSE</p>
-    </div>
+      <div className="names">
+        <div>
+          <p><b>BY:</b></p>
+          <p>ASHWINI KRISHNAJI RAO (1JS21CS030)</p>
+          <p>AISHWARYA S PATIL (1JS21CS013)</p>
+        </div>
+        <div>
+          <p><b>GUIDED BY:</b></p>
+          <p>MRS. RASHMI B N, B.E., M.TECH</p>
+          <p>ASSISTANT PROF, DEPT OF CSE</p>
+        </div>
+      </div>
+      <button className='My_button'><Link to="/app" className="project-link">CLICK TO SEE THE DEMO</Link>
+      </button></div>
+
   );
 };
 
